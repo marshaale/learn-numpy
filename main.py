4d54126c0815a596d570d82de8076f81eb5e2148
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-06-06 21:41:20 +0300

    basic index and slicing chain,multidimensional

--- main.py
+++ main.py
@@ -20,9 +20,36 @@ print(prices)
 
 prefix = np.array(252)
 
-scores = np.array([[97,88],[29,81],[30,78]])
+scores = np.array([
+    [97,88],
+    [29,81],
+    [30,78]
+    ])
+
 print(scores)
 
 print("Prefix",prefix,"number of dims",prefix.ndim)
 print("Numbers ",numbers,"number of dims",numbers.ndim)
 print("Scores\n",scores,"\nnumber of dimensions",scores.ndim)
+
+# chain indexing
+print(scores[0][0])
+print(scores[0][1])
+print(scores[1][0])
+
+# get entire row
+print(scores[0])
+
+# multi dimensional indexing
+print(scores[0,1])
+print(scores[1,0])
+print(scores[2,0])
+
+# slicing  index 0 then slicing : all | -> first row each all items
+print(scores[0,:])
+
+#  index : all and slicing : all | -> all for all
+print(scores[:,:])
+
+#  index : all and splicing 0 | -> all row each item index 0
+print(scores[:,0])
