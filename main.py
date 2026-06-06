e3161f9f3c2badd4dbfbcb8f6ed2ca9c993bdde9
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-06-06 20:50:04 +0300

    basic different btw numpy and  python list

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import numpy as np
 
-print(np.__version__)
+# print(np.__version__)
+
+numbers = np.array([1,2,3,4,5,6])
+print(numbers)
+
+print("Type",type(numbers))
+
+# squares 
+numbers = numbers * 2
+print(numbers)
+
+prices = [10,15,11,20]
+print(prices)
+
+# concat
+prices = prices*2
+print(prices)
+
 
